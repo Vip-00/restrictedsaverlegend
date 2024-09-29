906b0bc7b2f139e24fff9a6efe26147870dd3e17
Vip-00 <mrspect0011@gmail.com>
2024-09-30 01:20:47 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -20,7 +20,7 @@ API_HASH = config("d97919fb0a3c725e8bb2a25bbb37d57c", default=None)
 BOT_TOKEN = config("7066937947:AAEqiZ95P9X5Xjd_Dj801ef1gN7Ij1Uvtvc", default=None)
 SESSION = config("AQFDLj4AwVJQxc4eJUHWn9ZTGW30o-HHgs_xR6fiolhw5Y-a0AOXT6v0lmwAi9VoAbp_gH05uMhon9BrB0RjWwwk1eY3eZ_WBzpHiV0bMwQinGRQlYiiORqgHiFzW17uT_6SLsmd03gtIzicFjun3lXO0TfSogSw5FnbQVxuTyMzaXTRmzPdl89UuBt2hU_LC-cLOac8jErdnc_01OyqS5uceC59tIo-r8k8YoCVBXfPP2faysgAXPKNHZfrlGNlnwore-5yRdkWfGMweEH1tj4Bes1fIXN10iVkfJjJwPuq1w6R_wglb47HvRtlT6ajHLThZl6ehCJMSd53gaRJKKFHlHAGrwAAAAG0r_Q_AA", default=None)
 FORCESUB = config("@scammer_botxz", default=None)
-AUTH = config("7326397503", default=None)
+AUTH = config("2032347579", default=None)
 SUDO_USERS = []
 
 if len(AUTH) != 0:
